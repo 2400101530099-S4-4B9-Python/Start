6e0a705f0106176f39971cb3c983f5180361575a
PRINCE SINGH <princesinghsingh771@gmail.com>
2026-03-10 07:41:40 +0000

    JSX file make and create function

---
+++ start/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {Demo} from './Component/Demo'
+
+function App() {
+  
+
+  return (
+    <div>
+    <h1>This is Starting</h1>
+    <Example></Example>
+    <Demo></Demo>
+    
+    </div>
+  )
+  function Example()
+  {
+    return(
+    <h1>JAVA</h1>
+    )
+  }
+}
+
+export default App
+
---
+++ start/src/Component/Demo.jsx
@@ -0,0 +1,7 @@
+export function Demo()
+{
+    return(
+        <h1>This is Example of Demo</h1>
+    )
+}
+
