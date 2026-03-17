d8ea887775eaae0934f55f3937256b0eaee12d6e
PRINCE SINGH <princesinghsingh771@gmail.com>
2026-03-17 07:43:01 +0000

    Curly braces

--- start/src/App.jsx
+++ start/src/App.jsx
@@ -2,26 +2,33 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {Demo} from './Component/Demo'
 
 function App() {
-  
+  const name="PRINCE";
+  var x=10;
+  var y=20;
+  function fun()
+  {
+    return "Prince";
+  }
+  function sum(a,b)
+  {
+    return a+b;
+  }
 
+  function calc(a,b,op)
+  {
+
+  } 
   return (
     <div>
-    <h1>This is Starting</h1>
-    <Example></Example>
-    <Demo></Demo>
-    
+    <h1>User Variable with JSX</h1>
+    <h3>Value of Variable is {name}</h3>
+    <h3>Add of x and y = {x+y}</h3>
+    <h1>Use of funtion with JSX</h1>
+    <h3>Example of simple function {fun()}</h3>
+    <h3>Example of Parametrised function {sum(20,30)}</h3>
     </div>
   )
-  function Example()
-  {
-    return(
-    <h1>JAVA</h1>
-    )
-  }
 }
-
 export default App
-
