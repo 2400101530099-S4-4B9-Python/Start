9f27d5128b12995c09d66847c0457baca8c2b7cb
PRINCE SINGH <princesinghsingh771@gmail.com>
2026-03-31 07:32:52 +0000

    JS TO REACT

--- start/src/App.jsx
+++ start/src/App.jsx
@@ -4,31 +4,25 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const name="PRINCE";
-  var x=10;
-  var y=20;
-  function fun()
-  {
-    return "Prince";
-  }
-  function sum(a,b)
-  {
-    return a+b;
-  }
-
-  function calc(a,b,op)
-  {
-
-  } 
-  return (
-    <div>
-    <h1>User Variable with JSX</h1>
-    <h3>Value of Variable is {name}</h3>
-    <h3>Add of x and y = {x+y}</h3>
-    <h1>Use of funtion with JSX</h1>
-    <h3>Example of simple function {fun()}</h3>
-    <h3>Example of Parametrised function {sum(20,30)}</h3>
-    </div>
+  function CallFun()
+        {
+            alert("Function Call")
+        }
+  function Banana()
+        {
+            alert("You are click on BANANA button")
+        }
+   function Apple()
+        {
+            alert("You are click on APPLE button")
+        }     
+  return(
+<>
+    <button onClick={CallFun}>OK</button>
+    <button onClick={Banana}>BANANA</button>
+    <button onClick={Apple}>APPLE</button>
+    <h1>WELCOME TO EVENT HANDLING</h1>
+</>
   )
 }
 export default App
